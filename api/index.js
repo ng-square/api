079f3d2b6eecb6fa0d5745b7968d2cf2538d284c
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2024-07-20 21:25:11 +0200

    add tennis legends and pizza back

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.static("public"));
 
-app.get("/api/players", (req, res) => {
+app.get("/api/tennis/players", (req, res) => {
   res.json([
     {
       id: 936851900,
@@ -38,7 +38,7 @@ app.get("/api/players", (req, res) => {
   ]);
 });
 
-app.post("/api/players", (req, res) => {
+app.post("/api/tennis/players", (req, res) => {
   res.status(200).send();
 });
 
@@ -105,6 +105,45 @@ app.get("/api/tennis/legends", (req, res) => {
       grandSlamWins: 7,
     },
   ]);
+});
+
+app.get("/api/pizza", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: "Margherita",
+      description: "Tomato sauce, mozzarella, organic oregano",
+      price: 16,
+      image: "src/app/assets/Margherita.jpeg",
+    },
+    {
+      id: 2,
+      name: "Stromboli",
+      description:
+        "Tomato sauce, mozzarella, fresh chillies, olives, organic oregano",
+      price: 18,
+      image: "src/app/assets/Margherita.jpeg",
+    },
+    {
+      id: 3,
+      name: "Napoli",
+      description:
+        "Tomato sauce, mozzarella, anchovies MSC, capers, organic oregano",
+      price: 19,
+      image: "src/app/assets/Margherita.jpeg",
+    },
+    {
+      id: 4,
+      name: "Funghi",
+      description: "Tomato sauce, mozzarella, fresh mushrooms, organic oregano",
+      price: 20,
+      image: "src/app/assets/Margherita.jpeg",
+    },
+  ]);
+});
+
+app.post("/api/pizza", (req, res) => {
+  res.status(200).send();
 });
 
 app.listen(process.env.PORT || 3000);
